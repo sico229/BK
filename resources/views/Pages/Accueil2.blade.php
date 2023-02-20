c5cecfb866f997693c9da3a2d22835453c8193cb
sico <serge.aguidissou@gmail.com>
2023-02-20 17:33:08 +0100

    Page d'accueil du template 2

--- resources/views/Pages/Accueil2.blade.php
+++ resources/views/Pages/Accueil2.blade.php
@@ -12,27 +12,37 @@
                 <div>
                     <p><i class="bi bi-keyboard"></i></p>
                     <p>100% accessible en ligne</p>
+                    <p>Accédez à votre compte en ligne depuis votre smartphone ou votre ordinateur</p>
                     <p><a href="">Ouvrir un Compte</a></p>
                 </div>
-                <div>B</div>
+                <div>
+                    <p>Sans bouger de chez vous, ouvrez votre compte en ligne et effectuez toutes vos opérations bancaires
+                    </p>
+                </div>
             </div>
             <div>
                 <div>
                     <p><i class="bi bi-award"></i></p>
                     <p>Meilleurs Services</p>
+                    <p>Nous vous offrons des solutions inovantes</p>
                     <p><a href="">Ouvrir un Compte</a></p>
 
                 </div>
-                <div>B</div>
+                <div>
+                    <p>Un service client dédié vous accompagne 24h/24</p>
+                </div>
             </div>
             <div>
                 <div>
                     <p><i class="bi bi-shield-fill-check"></i></p>
                     <p>Services Securisés</p>
+                    <p>Vos opérations bancaires sont couvertes à 100%</p>
                     <p><a href="">Ouvrir un Compte</a></p>
 
                 </div>
-                <div>B</div>
+                <div>
+                    <p>Nous utilisons les dernières technologies de sécurité sur nos plateformes</p>
+                </div>
             </div>
 
         </div>
